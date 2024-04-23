a387b3b7d0b23fd6ac7530ab42a398e036555370
boringAdam <mikeadam19999@gmail.com>
2024-04-24 01:51:06 +0200

    Created /doc endpoint for documentation. (The documentation is not ready yet)

--- src/main/java/com/filetree/controller/FileTreeController.java
+++ src/main/java/com/filetree/controller/FileTreeController.java
@@ -5,8 +5,10 @@ import com.filetree.dto.LogEntryDTO;
 import com.filetree.service.LogEntryService;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
@@ -17,17 +19,31 @@ import java.util.Random;
 import java.util.Set;
 import java.time.LocalDateTime;
 
-@RestController
+/**
+ * The FileTreeController class handles all the endpoints
+ */
+@Controller
 public class FileTreeController {
 
     private final LogEntryService logEntryService;
 
+    /**
+     * Constructor to initialize the FileTreeController
+     * @param logEntryService The LogEntryService the to log the callings of the /getunique endpoint.
+     */
     @Autowired
     public FileTreeController(LogEntryService logEntryService) {
         this.logEntryService = logEntryService;
     }
 
+    /**
+     * Endpoint to retrieve a set of unique filenames matching the specified extension.
+     * @param rootFolder The root folder to search.
+     * @param extension  The file extension to match.
+     * @return Returns with the list of the distinct filenames.
+     */
     @GetMapping("/getunique")
+    @ResponseBody
     public Set<String> findFiles(@RequestParam String rootFolder, @RequestParam String extension) {
         Set<String> filenames = new HashSet<>();
         findFiles(new File(rootFolder), extension, filenames);
@@ -40,11 +56,18 @@ public class FileTreeController {
     }
 
     @GetMapping("/history")
+    @ResponseBody
     public List<LogEntryDTO> getHistory() {
         return logEntryService.getAllLogEntries();
     }
 
+    @GetMapping("/doc")
+    public String getDocumentation(){
+        return "redirect:/";
+    }
+
     @GetMapping("/gen")
+    @ResponseBody
     public String generateFileStructure() {
         File rootFolder = new File("generated_root");
 
